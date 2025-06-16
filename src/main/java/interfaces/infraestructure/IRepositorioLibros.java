6c47f926b7f1f7079d8fa2ba4f6062cf91e7b564
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-06-15 22:48:42 -0400

    Feat: Implement CSV data loading for books and users on startup

--- src/main/java/interfaces/infraestructure/IRepositorioLibros.java
+++ src/main/java/interfaces/infraestructure/IRepositorioLibros.java
@@ -6,8 +6,9 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 public interface IRepositorioLibros {
-    ArrayList<Libro> buscarLibroPorTitulo(String titulo);
+    ArrayList<Libro> buscarLibros(String criterio); // Renamed and purpose changed
     void agregarLibro(Libro libro);
     void eliminarLibro(Libro libro);
     Optional<Libro> buscarLibroPorId(String id);
+    ArrayList<Libro> obtenerTodosLosLibros();
 }
